6c52b77288c41704f8758a253d9ed6da2d12458b
Link1515 <h26281660@gmail.com>
2024-11-20 14:02:56 +0800

    use debounce while searching

--- source/js/partials/base/search.js
+++ source/js/partials/base/search.js
@@ -1,19 +1,57 @@
-const searchInputEl = document.querySelector("#searchInput");
-const searchResultsEl = document.querySelector("#searchResults");
-const postTitlesEl = document.querySelector("#postTitles");
+const searchInputEl = document.querySelector('#searchInput');
+const searchResultsEl = document.querySelector('#searchResults');
+const searchNoResultEl = document.querySelector('#searchNoResult');
+const noResultEl = document.querySelector('#searchResults #noResult');
+const postTitlesEl = document.querySelector('#postTitles');
 const postTitles = JSON.parse(postTitlesEl.dataset.postTitles) || [];
 
 const fuse = new window.Fuse(postTitles, { keys: ['title'] });
 
-searchInputEl.addEventListener("input", (event) => {
-  const searchPattern = event.target.value;
-  const results = fuse.search(searchPattern);
-  appendSearchResults(results);
-});
+searchInputEl.addEventListener(
+  'input',
+  window.debounce(
+    event => {
+      hideNoResult();
+
+      const searchPattern = event.target.value;
+      if (!searchPattern) {
+        clearSearchResults();
+        return;
+      }
+
+      const results = fuse.search(searchPattern);
+      if (results.length === 0) {
+        clearSearchResults();
+        showNoResult();
+        return;
+      }
+      appendSearchResults(results);
+    },
+    { leading: true }
+  )
+);
+
+function clearSearchResults() {
+  searchResultsEl.innerHTML = '';
+}
 
 function appendSearchResults(results) {
-  const html = results.map(result => `
-    <a href="${result.item.link}" class="list-group-item list-group-item-action">${result.item.title}</a>
-  `).join("");
+  const html = results
+    .map(
+      result => `
+        <a href="${result.item.link}" class="list-group-item list-group-item-action">
+          ${result.item.title}
+        </a>
+      `
+    )
+    .join('');
   searchResultsEl.innerHTML = html;
+}
+
+function showNoResult() {
+  searchNoResultEl.style.display = 'block';
+}
+
+function hideNoResult() {
+  searchNoResultEl.style.display = 'none';
 }
--- source/js/utils.js
+++ source/js/utils.js
@@ -1,10 +1,17 @@
-function debounce (fn, delay = 300) {
+function debounce(fn, { delay = 300, leading = false } = {}) {
   let timer;
+  let hasCalledLeading = false;
 
   return (...args) => {
+    if (leading && !hasCalledLeading) {
+      fn(...args);
+      hasCalledLeading = true;
+    }
+
     clearTimeout(timer);
 
     timer = setTimeout(() => {
+      hasCalledLeading = false;
       fn(...args);
     }, delay);
   };
