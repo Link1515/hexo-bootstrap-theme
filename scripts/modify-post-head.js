60edce7d6f7b55e886869786984a9f4876181784
Link1515 <h26281660@gmail.com>
2024-11-21 16:15:42 +0800

    add script: modify-post-head

---
+++ scripts/modify-post-head.js
@@ -0,0 +1,65 @@
+hexo.extend.filter.register('after_render:html', function (html, data) {
+  html = changeHeadTitle(html, data.config.title, data.page.title);
+
+  if (data.page.cover) {
+    html = changeHeadImage(html, data.page.cover);
+  }
+
+  if (data.page.description) {
+    html = changeHeadDescription(html, data.page.description);
+  }
+
+  return html;
+});
+
+function changeHeadTitle(html, title, postTitle) {
+  html = html.replace(
+    /(<title>).*?(<\/title>)/,
+    (_, start, end) => `${start}${postTitle} | ${title}${end}`
+  );
+
+  html = html.replace(
+    /(<meta property="og:title" content=").*?(" \/>)/,
+    (_, start, end) => `${start}${postTitle} | ${title}${end}`
+  );
+
+  html = html.replace(
+    /(<meta name="twitter:title" value=").*?(" \/>)/,
+    (_, start, end) => `${start}${postTitle} | ${title}${end}`
+  );
+
+  return html;
+}
+
+function changeHeadImage(html, cover) {
+  html = html.replace(
+    /(<meta property="og:image" content=").*?(" \/>)/,
+    (_, start, end) => `${start}${cover}${end}`
+  );
+
+  html = html.replace(
+    /(<meta name="twitter:image" value=").*?(" \/>)/,
+    (_, start, end) => `${start}${cover}${end}`
+  );
+
+  return html;
+}
+
+function changeHeadDescription(html, description) {
+  html = html.replace(
+    /(<meta name="description" content=").*?(" \/>)/,
+    (_, start, end) => `${start}${description}${end}`
+  );
+
+  html = html.replace(
+    /(<meta property="og:description" content=").*?(" \/>)/,
+    (_, start, end) => `${start}${description}${end}`
+  );
+
+  html = html.replace(
+    /(<meta name="twitter:description" value=").*?(" \/>)/,
+    (_, start, end) => `${start}${description}${end}`
+  );
+
+  return html;
+}
