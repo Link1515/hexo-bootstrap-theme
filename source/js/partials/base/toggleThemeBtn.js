4c8e26b2ef07f420ba13677d2d572b5b1a209baa
Link1515 <h26281660@gmail.com>
2024-11-22 09:41:12 +0800

    toggleThemeBtn use single icon to avoid refresh flash issue

--- source/js/partials/base/toggleThemeBtn.js
+++ source/js/partials/base/toggleThemeBtn.js
@@ -1,27 +1,6 @@
 const toggleThemeBtnEl = document.querySelector('#toggleThemeBtn');
-const sunIconEl = document.querySelector('#sunIcon');
-const moonIconEl = document.querySelector('#moonIcon');
 
-initTheme();
-
-function initTheme() {
-  const theme = window.localStorage.getItem('theme') ?? '';
-  document.body.dataset.bsTheme = theme;
-
-  theme === ''
-    ? sunIconEl.classList.remove('d-none')
-    : moonIconEl.classList.remove('d-none');
-}
-
-toggleThemeBtnEl.addEventListener('click', () => {
-  toggleBtnIcon();
-  toggleTheme();
-});
-
-function toggleBtnIcon() {
-  sunIconEl.classList.toggle('d-none');
-  moonIconEl.classList.toggle('d-none');
-}
+toggleThemeBtnEl.addEventListener('click', toggleTheme);
 
 function toggleTheme() {
   const theme = document.body.dataset.bsTheme;
